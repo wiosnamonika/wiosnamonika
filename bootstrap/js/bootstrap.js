3f4f77de35309855b6ba8b1ee3b3ae3d6d39992b
Monika Wiosna <mwiosna.zielinska@gmail.com>
2017-09-01 00:52:52 +0200

    bootstrap projekt

--- bootstrap/js/bootstrap.js
+++ bootstrap/js/bootstrap.js
@@ -1,49 +1,54 @@
-$('.carousel').carousel({
-  interval: 3500;
-})
+//$('.carousel').carousel({
+//  setInterval: 3500;
+//})
 
-//google api AIzaSyBJZbiDVQr5xoPDEjTBA14s5rFTDwuvJEA
+$(window).load(function () {
+        var st;
 
+        $('#carousel').on('slid.bs.carousel', function () {
+         //   debugger;
 
-//var carousel = $('ol.carousel-indicators').children();
-//var dealer = $('.dealer');
-//
-//if (carousel == )
+            var newinterval = $('#carousel .carousel-inner').find('.active').attr('interval');
 
-//var script = document.createElement('script');
-//script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
-//        document.getElementsByTagName('head')[0].appendChild(script);
+            $('#carousel').carousel("pause");
+            clearTimeout(st);
+           st =  setTimeout("$('#carousel').carousel()", newinterval);
 
-//<script>
-//      var map;
-//      function initMap() {
-//        map = new google.maps.Map(document.getElementById('map'), {
-//          zoom: 2,
-//          center: new google.maps.LatLng(2.8,-187.3),
-//          mapTypeId: 'terrain'
-//        });
-//
-//        // Create a <script> tag and set the USGS URL as the source.
-//        var script = document.createElement('script');
-//        // This example uses a local copy of the GeoJSON stored at
-//        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-//        script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-//        document.getElementsByTagName('head')[0].appendChild(script);
-//      }
-//
-//      // Loop through the results array and place a marker for each
-//      // set of coordinates.
-//      window.eqfeed_callback = function(results) {
-//        for (var i = 0; i < results.features.length; i++) {
-//          var coords = results.features[i].geometry.coordinates;
-//          var latLng = new google.maps.LatLng(coords[1],coords[0]);
-//          var marker = new google.maps.Marker({
-//            position: latLng,
-//            map: map
-//          });
-//        }
-//      }
-//    </script>
-//    <script async defer
-//    src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
-//    </script>
+           // $('#carousel').carousel({ interval: newinterval });
+        });
+    
+    
+    
+    
+    $(document).ready(function(){
+
+             $('#contact').validate(
+             {
+              rules: {
+                name: {
+                  minlength: 2,
+                  required: true
+                },
+                email: {
+                  required: true,
+                  email: true
+                },
+                message: {
+                  minlength: 2,
+                  required: true
+                }
+              },
+              highlight: function(element) {
+                $(element).closest('.control-group').removeClass('success').addClass('error');
+              },
+              success: function(element) {
+                element
+                .text('OK!').addClass('valid')
+                .closest('.control-group').removeClass('error').addClass('success');
+              }
+             });
+            }); 
+    
+    
+    
+    });
