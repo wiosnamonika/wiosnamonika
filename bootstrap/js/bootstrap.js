d8afd80f718d2a6518d208dcc7a984e99e5ffca2
Monika Wiosna <mwiosna.zielinska@gmail.com>
2017-09-01 00:54:00 +0200

    wciecia

--- bootstrap/js/bootstrap.js
+++ bootstrap/js/bootstrap.js
@@ -3,52 +3,51 @@
 //})
 
 $(window).load(function () {
-        var st;
+    var st;
 
-        $('#carousel').on('slid.bs.carousel', function () {
-         //   debugger;
+    $('#carousel').on('slid.bs.carousel', function () {
+        //   debugger;
 
-            var newinterval = $('#carousel .carousel-inner').find('.active').attr('interval');
+        var newinterval = $('#carousel .carousel-inner').find('.active').attr('interval');
 
-            $('#carousel').carousel("pause");
-            clearTimeout(st);
-           st =  setTimeout("$('#carousel').carousel()", newinterval);
+        $('#carousel').carousel("pause");
+        clearTimeout(st);
+        st = setTimeout("$('#carousel').carousel()", newinterval);
 
-           // $('#carousel').carousel({ interval: newinterval });
-        });
-    
-    
-    
-    
-    $(document).ready(function(){
+        // $('#carousel').carousel({ interval: newinterval });
+    });
 
-             $('#contact').validate(
-             {
-              rules: {
+
+
+
+    $(document).ready(function () {
+
+        $('#contact').validate({
+            rules: {
                 name: {
-                  minlength: 2,
-                  required: true
+                    minlength: 2,
+                    required: true
                 },
                 email: {
-                  required: true,
-                  email: true
+                    required: true,
+                    email: true
                 },
                 message: {
-                  minlength: 2,
-                  required: true
+                    minlength: 2,
+                    required: true
                 }
-              },
-              highlight: function(element) {
+            },
+            highlight: function (element) {
                 $(element).closest('.control-group').removeClass('success').addClass('error');
-              },
-              success: function(element) {
+            },
+            success: function (element) {
                 element
-                .text('OK!').addClass('valid')
-                .closest('.control-group').removeClass('error').addClass('success');
-              }
-             });
-            }); 
-    
-    
-    
+                    .text('OK!').addClass('valid')
+                    .closest('.control-group').removeClass('error').addClass('success');
+            }
+        });
     });
+
+
+
+});
