43820909c1a3b01426aaa489cea1a2edba576175
Monika Wiosna <mwiosna.zielinska@gmail.com>
2017-10-13 16:42:57 +0200

    corrections

--- Projekty/js/app.jsx
+++ Projekty/js/app.jsx
@@ -2,39 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class TextIntro extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      text: TextIntro.text,
-    
-        
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: TextIntro.text,
 
-    this.counter = 0;
-  }
-  componentDidMount() {
-    this.timerId = setInterval(()=>{
-      this.counter++
-      this.setState({
-        text: this.props.text.slice(0, this.counter)
-      });
 
-    },500)
-  }
-  componentWillUnmount(){
-    clearInterval(this.timerId);
-  }
-  render(){
-    return (
-        <h1>{this.state.text}</h1>
+        };
 
-    );
-  }
+        this.counter = 0;
+    }
+    componentDidMount() {
+        this.timerId = setInterval(() => {
+            this.counter++
+                this.setState({
+                    text: this.props.text.slice(0, this.counter)
+                });
+
+        }, 500)
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+    render() {
+        return ( <
+            h1 > {
+                this.state.text
+            } < /h1>
+
+        );
+    }
 }
-document.addEventListener("DOMContentLoaded", ()=>{
-  ReactDOM.render(
-    <TextIntro text='Choose your road...!'/>,
+document.addEventListener("DOMContentLoaded", () => {
+    ReactDOM.render( <
+        TextIntro text = 'Choose your road...!' / > ,
 
-    document.getElementById('app')
-  );
+        document.getElementById('app')
+    );
 });
