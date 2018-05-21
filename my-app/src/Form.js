7c9933b90c7f6ba6b7699765915dec78702d5cff
Monika Wiosna <mwiosna.zielinska@gmail.com>
2018-05-21 13:45:32 +0200

    form

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -1,198 +1,141 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import axios from 'axios';
-import cx from 'classnames';
-import TextareaAutosize from 'react-textarea-autosize';
+import { Errors } from './Errors';
 
-const ERROR_TYPE_SERVER = 'server_error';
-const ERROR_TYPE_DOMAIN = 'domain_error'; // eslint-disable-line no-unused-vars
-const ERROR_TYPE_GENERAL = 'general_error';
-
-const RESPONSE_TYPE_ERROR = 'error';
-const RESPONSE_TYPE_SUCCESS = 'success';
-
-class CareersForm extends Component {
-    constructor(props) {
+class Form extends Component {
+    constructor (props) {
         super(props);
-
-        this.initialState = {
-            password: '',
+        this.state = {
             email: '',
-            error: {
-                password: false,
-                email: false,
+            password: '',
+            agree: false,
+            errors: {
+                email: '',
+                password: '',
+                agree: false,
             },
-            sending: false,
-            responseType: null,
-            errorType: null
-        };
-
-        this.state = { ...this.initialState };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.fileClear = this.fileClear.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    fileClear(field) {
-        return () => {
-            const elem = document.getElementById(`careers-form-${ field }`);
-            if (elem) {
-                elem.value = null;
-            }
-            this.setState({ [field]: null });
-        };
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-
-        if (this.state.sending) {
-            return;
+            emailValid: false,
+            passwordValid: false,
+            formValid: false
         }
 
-        const {
-            password, email
-        } = this.state;
+        this.handleChangeCheckbox = this.handleChangeCheckbox.bind(this);
+        this.handleUserInput = this.handleUserInput.bind(this);
+    }
 
-        const error = {
-            password: false,
-            email: false,
-        };
+    handleUserInput(e) {
+        const name = e.target.name;
+        const value = e.target.value;
+        const agree = e.target.checked;
+        this.setState({[name]: value}, () => { this.validation(name, value) });
+    }
 
-        const passwordRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/;
-        // error.password = (password === '');
-        error.password = !passwordRegExp.test(password);
-        const emailRegExp = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/; // eslint-disable-line max-len
-        error.email = !emailRegExp.test(email);
-
-        const errorAny = Object.keys(error).reduce((acc, key) => error[key] || acc, false);
-        if (errorAny === false) {
-            this.setState({ error, sending: true });
-
-            const data = new FormData();
-            data.append('password', name.substring(0, 50));
-            data.append('email', email.substring(0, 100));
-
-            axios({
-                method: 'post',
-                url: `${ process.env.PUBLIC_URL }/code/form-careers.php`,
-                data,
-                headers: {
-                    'X-Requested-With': 'XMLHttpRequest'
-                }
-            })
-                .then((response) => {
-                    if (response.data && response.data.success) {
-                        this.setState({
-                            ...this.initialState,
-                            responseType: 'success'
-                        });
-                        setTimeout(() => {
-                            this.setState({ responseType: null });
-                        }, 5000);
-                    } else {
-                        this.setState({
-                            error: {
-                                password: !!response.data.error_name,
-                                email: !!response.data.error_email
-                            },
-                            sending: false,
-                            responseType: RESPONSE_TYPE_ERROR,
-                            errorType: response.data.error
-                        });
-                    }
-                })
-                .catch(() => {
-                    this.setState({ sending: false, responseType: RESPONSE_TYPE_ERROR, errorType: ERROR_TYPE_SERVER });
-                });
+    handleChangeCheckbox(e) {
+        if (e.target.type === 'checkbox') {
+            return this.setState({ [e.target.name]: e.target.checked });
         } else {
-            this.setState({ error, responseType: RESPONSE_TYPE_ERROR, errorType: ERROR_TYPE_GENERAL });
+            this.setState({ [e.target.name]: e.target.value });
         }
     }
 
-    render() {
-        const {
-            password, email, error, responseType, errorType, sending
-        } = this.state;
+    validation(fieldName, value) {
+        let validationErrors = this.state.errors;
+        let emailValid = this.state.emailValid;
+        let passwordValid = this.state.passwordValid;
+
+    switch(fieldName) {
+        case 'email':
+            emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+            validationErrors.email = emailValid ? '' : ' is invalid';
+        break;
+        case 'password':
+            passwordValid = value.length >= 6;
+            validationErrors.password = passwordValid ? '': ' is invalid';
+        break;
+        default:
+        break;
+    }
+    this.setState({
+        errors: validationErrors,
+        emailValid: emailValid,
+        passwordValid: passwordValid
+    },
+    this.validateForm);
+  }
+
+    validateForm() {
+        this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+  }
+
+    errorClass(error) {
+        return(error.length === 0 ? '' : 'has-error');
+  }
+
+
+    render () {
 
         return (
-            <div className="careers-list-contact-box">
-                <h3 className="careers-list-contact-title">
-                    title
-                </h3>
-                <form
-                  method="post"
-                  action=""
-                  acceptCharset="utf-8"
-                  className="careers-list-contact-form"
-                  onSubmit={ this.handleSubmit }
-                  noValidate
-                >
-                    <div className="input-row">
-                        <div className="input-box input-box-50">
-                            <input
-                              type="password"
-                              name="password"
-                              id="careers-form-name"
-                              value={ password }
-                              maxLength={ 20 }
-                              className={ cx('text-input', { 'not-empty': (password !== ''), error: error.password }) }
-                              onChange={ this.handleChange }
-                            />
-                            <label className="text-label" htmlFor="careers-form-name">
-                                name
-                            </label>
-                        </div>
-                    </div>
-                    <div className="input-box">
-                        <input
-                          type="email"
-                          name="email"
-                          id="careers-form-email"
-                          value={ email }
-                          maxLength={ 50 }
-                          className={ cx('text-input', { 'not-empty': (email !== ''), error: error.email }) }
-                          onChange={ this.handleChange }
-                        />
-                        <label className="text-label" htmlFor="careers-form-email">
-                            email
-                        </label>
-                    </div>
+            <form
+                method="post"
+                action=""
+                acceptCharset="utf-8"
+                className="contact-form"
+                // noValidate
+            >
+                <h2>Sign up</h2>
+                <div className="panel panel-default">
+                    <Errors errors={ this.state.errors } />
+                </div>
+                <div className={ `input-row ${this.errorClass(this.state.errors.email) }`}>
+                    <label htmlFor="email">Email address</label>
+                    <input
+                      type="email"
+                      required
+                      className="form-control"
+                      name="email"
+                      placeholder="Email"
+                      value={this.state.email}
+                      onChange={this.handleUserInput}  />
+                </div>
+                <div className={ `input-row ${this.errorClass(this.state.errors.password) }`}>
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      name="password"
+                      placeholder="Password"
+                      value={ this.state.password }
+                      onChange={ this.handleUserInput }
+                      pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
 
+                />
+                </div>
+                <div className="checkbox-input input-row">
+                    <label htmlFor="email">Remember me</label>
+                    <input
+                      type="checkbox"
+                      name="agree"
+                      id="form-agree"
+                      className="checkbox-input"
+                      onChange={ this.handleCheckbox }
+                    />
+                </div>
+                <div className="login-button input-row">
+                    <button
+                      type="submit"
+                      value="login"
+                      className="login-button "
+                      disabled={ !this.state.formValid }
+                    >
                     {
-                        responseType !== RESPONSE_TYPE_SUCCESS &&
-                            <div className="button-box">
-                                <button type="submit" className="button" disabled={ sending }>
-                                    {
-                                        sending
-                                            ? 'issending'
-                                            : 'not sending'
-                                    }
-                                </button>
-                            </div>
+                        this.state.formValid ? (
+                            <span className="login-button-text">Log in</span>
+                        ) && "success" : null
                     }
-                    {
-                        responseType === RESPONSE_TYPE_SUCCESS &&
-                            <div className={ cx('form-message', responseType) }>
-                                <span>'sukces'</span>
-                            </div>
-                    }
-                    {
-                        responseType === RESPONSE_TYPE_ERROR &&
-                            <div className={ cx('form-message', 'error') }>
-                                <span>'nie wyslano'${ errorType }</span>
-                            </div>
-                    }
-                </form>
-            </div>
-        );
+                    </button>
+                </div>
+            </form>
+        )
     }
 }
 
-CareersForm.propTypes = {
-    router: PropTypes.shape().isRequired,
-    t: PropTypes.func.isRequired
-};
-
-export default CareersForm;
+export default Form;
